83e552feaa87ef6d69fdb8775f99c0ce5a06a188
hyfjsw001 <32854824+hyfjsw001@users.noreply.github.com>
2017-10-25 13:24:23 +0800

    Add files via upload

---
+++ VarDemo.java
@@ -0,0 +1,23 @@
+//Day2
+//java字符类型;变量必须要有初始值。{}作用域有作用。{{}}。
+
+
+class  VarDemo
+{
+	public static void main(String[] args) 
+	{
+		//数据类型 变量名=初始化值；
+		byte b=3;
+		short s=6000;
+		int q=34;
+		long t=213656546514121315L;//long型要数的末尾加L
+		float f=2.1f;//同上要加声明F
+		double d=3.4;
+		char ch='a';
+		boolean bl=true;
+		bl=false;
+
+
+		System.out.println(f);
+	}
+}
---
+++ VarDemo2.java
@@ -0,0 +1,9 @@
+class VarDemo2 
+{
+	public static void main(String[] args) 
+	{
+		int x=3;
+		x=x+5;
+		System.out.println(x);
+	}
+}
