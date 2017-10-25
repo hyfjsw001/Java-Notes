62e5c1eaf448bc0431cb7b6db4c0878a085f1913
hyfjsw001 <32854824+hyfjsw001@users.noreply.github.com>
2017-10-25 13:53:06 +0800

    Update VarDemo2.java

--- VarDemo2.java
+++ VarDemo2.java
@@ -2,8 +2,14 @@ class VarDemo2
 {
 	public static void main(String[] args) 
 	{
-		int x=3;
-		x=x+5;
+		//数值型和非数值型，数值型之间可以运算。自动类型提升，内存。
+		//int x=3;
+		//x=x+5;
+		//byte b=5;
+		//x=x+b
+		byte b=3;
+		b=(byte)(b+4);//强制类型转换。多变少会损失精度
 		System.out.println(x);
 	}
 }
+//负数最高位是1
